e063033606ff17ec71dafe4a9c46af117a386207
Selim-Gunal <selim.g.2007@gmail.com>
2022-01-25 19:17:36 +0300

    First Question

--- 1.py
+++ 1.py
@@ -1,13 +1,14 @@
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
-i = 0
-x = 0
-for x in range(0 ,1000):
-    if (x % 3 == 0):
-        i = i + x
-    if (x % 5 == 0):
-        i = i + x
-        if (x % 3 == 0):
-            i = i - x
-print(i)
+sum = 0
+
+#I've done a for loop because the the question wants the numbers below the one thousand
+for i in range(0 ,1000):
+    if (i % 3 == 0):
+        sum = sum + i
+    if (i % 5 == 0):
+        sum = sum + i
+        if (i % 3 == 0):
+            sum = sum - i
+print(sum)
