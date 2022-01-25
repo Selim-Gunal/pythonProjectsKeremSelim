fb992f86f478314d86a7e2f09fd20a83b033ec17
Selim-Gunal <selim.g.2007@gmail.com>
2022-01-25 16:20:33 +0300

    Second Question

--- 2.py
+++ 2.py
@@ -6,22 +6,22 @@ x = 1
 y = 2
 z = 0
 u = 0
-sum_of_can_devide_two = 0
+sum_of_numbers_that_can_devide_two = 0
 
 #This while loop will give us a chance to get our numbers smaller than 4 million
 while (x < 4000000 and y < 4000000 and z < 4000000 and u < 4000000):
     z = x + y
     if (z % 2 == 0):
-        sum_of_can_devide_two = sum_of_can_devide_two + z
+        sum_of_numbers_that_can_devide_two = sum_of_numbers_that_can_devide_two + z
     u = y + z
     if (u % 2 == 0):
-        sum_of_can_devide_two = sum_of_can_devide_two + u
+        sum_of_numbers_that_can_devide_two = sum_of_numbers_that_can_devide_two + u
     x = z + u
     if (x % 2 == 0):
-        sum_of_can_devide_two = sum_of_can_devide_two + x
+        sum_of_numbers_that_can_devide_two = sum_of_numbers_that_can_devide_two + x
     y = u + x
     if (y % 2 == 0):
-        sum_of_can_devide_two = sum_of_can_devide_two + y
+        sum_of_numbers_that_can_devide_two = sum_of_numbers_that_can_devide_two + y
 
 #We've added two because at first we've started with 3 three
-print(sum_of_can_devide_two + 2)
+print(sum_of_numbers_that_can_devide_two + 2)
