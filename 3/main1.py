2652cfca6c9f2eca16f61a2380132df8cda8a2b2
Selim-Gunal <selim.g.2007@gmail.com>
2022-01-28 01:54:15 +0300

    3.py

---
+++ 3/main1.py
@@ -0,0 +1,9 @@
+the_number_s_text = open("./3/numbers.txt","w")
+
+our_number = 600851475143
+
+for i in range (2 ,3000000000):
+    if (our_number % i == 0):
+        the_number_s_text.write(str(i) + " ")
+
+the_number_s_text.close()
---
+++ 3/main3.py
@@ -0,0 +1,9 @@
+the_number_s_text = open("./3/numbers.txt","w")
+
+our_number = 600851475143
+
+for i in range (6000000000 ,8583592502):
+    if (our_number % i == 0):
+        the_number_s_text.write(str(i) + " ")
+
+the_number_s_text.close()
