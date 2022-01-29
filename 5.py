7d1aae00e5ffaf4b45c1cc9c8e7472775cb11d91
Selim-Gunal <selim.g.2007@gmail.com>
2022-01-29 22:28:44 +0300

    5

--- 5.py
+++ 5.py
@@ -3,6 +3,14 @@ numbers_list = []
 for i in range (1 ,21):
     numbers_list.append(i)
 
-for i in range (2 ,20):
-    for x in range (1 ,21):
-        
+for i in range (2 ,21):
+    for x in range (2 ,21):
+        if (i == x):
+            x = x + 1
+        if (x / i % 1 == 0):
+            for z in range (0 ,len(numbers_list)):
+                if (x == numbers_list[z]):
+                    remember_num = z
+                    del numbers_list[z]
+                    break
+print(numbers_list)
