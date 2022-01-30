8b2676cf07f529c34eede65b739efca4f8124630
Selim-Gunal <selim.g.2007@gmail.com>
2022-01-30 22:38:42 +0300

    6

--- 6.py
+++ 6.py
@@ -5,9 +5,19 @@
 
 sum_square = 0
 square_sum = 0
+for_square_sum = 0
 
 #Here we will find the sum of the squares of the first one hundred natural numbers
 for i in range (1 ,101):
     sum_square = sum_square + i ** 2
-print(sum_square)
 
+#Here i sum all the numbers below 101
+for i in range (1 ,101):
+    for_square_sum = for_square_sum + i
+#And then I get the square of it
+square_sum = for_square_sum ** 2
+
+#Lastly I sumtracted the smaller one from the bigger one
+if (sum_square > square_sum):
+    print(sum_square - square_sum)
+else:print(square_sum - sum_square)
