10c899564ffe0e8ee46f3e6700f6282b4128a342
Selim-Gunal <selim.g.2007@gmail.com>
2022-01-30 03:05:47 +0300

    5

--- 5.py
+++ 5.py
@@ -1,16 +1,24 @@
-numbers_list = []
+#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-for i in range (1 ,21):
-    numbers_list.append(i)
+sum_of_numbers = 1
+prime_numbers = []
+for_test = 0
 
 for i in range (2 ,21):
     for x in range (2 ,21):
+        for_test = 0
         if (i == x):
             x = x + 1
-        if (x / i % 1 == 0):
-            for z in range (0 ,len(numbers_list)):
-                if (x == numbers_list[z]):
-                    remember_num = z
-                    del numbers_list[z]
-                    break
-print(numbers_list)
+        if (i % x == 0):
+            for_test = -1
+            break
+    if (for_test == 0):
+        prime_numbers.append(i)
+
+for i in range (0 ,len(prime_numbers)):
+    for x in range (4 ,0 ,-1):
+        if (prime_numbers[i] ** x < 20):
+            sum_of_numbers = sum_of_numbers * prime_numbers[i] ** x
+            break
+print(sum_of_numbers)
