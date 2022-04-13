09649ab655f6d06cd148c9382aa5c8349fa500d0
Selim G <selim.g.2007"gmail.com>
2022-04-13 14:33:46 +0300

    cpp

--- 5_21.cpp
+++ 5_21.cpp
@@ -6,20 +6,14 @@ int main (){
     //a ,b ,c ,d Represents the shapes like a is the first shape
     int i ,a ,b ,c ,d ,cache;
     a = 1;
-    b = 10;
+    c = 10;
     for (i = 0 ;i < 10 ;i = i + 1) {
+
         for (cache = 0; cache < a; cache = cache + 1) {
             cout << "*";
         }
         a = a + 1;
         cout << "         ";
-        for (cache = 0 ;cache < b ;cache = cache + 1){
-            cout << "*";
-        }
-        b = b - 1;
-        cout << "         ";
-
-        for ()
 
         cout << endl;
     }
