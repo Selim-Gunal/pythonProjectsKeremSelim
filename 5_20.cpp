1034fc2456afd13d53e0343cb28d0b454e4921f7
Selim G <selim.g.2007"gmail.com>
2022-04-21 19:08:31 +0300

    cpp

---
+++ 5_20.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int sideOne ,sideTwo ,hypotenuse ,i ,indicateWhileLoop;
+    indicateWhileLoop = false;
+
+    while (indicateWhileLoop == false){
+
+    }
+}
