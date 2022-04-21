dcc6e38e4c6bb3ac01e05996d388afe950c6ace5
Selim G <selim.g.2007"gmail.com>
2022-04-21 20:42:24 +0300

    cpp

--- 5_20.cpp
+++ 5_20.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main (){
-    int sideOne ,sideTwo ,hypotenuse ,i ,indicateWhileLoop;
+    int sideOne ,sideTwo ,i ,x ,indicateWhileLoop;
+    float hypotenuse ,hypotenuseMod;
     indicateWhileLoop = false;
 
     while (indicateWhileLoop == false){
 
+        for (sideOne = 1 ;sideOne < 501 ;sideOne = sideOne + 1){
+
+            for (sideTwo = sideOne + 1 ;sideTwo < 501 ;sideTwo = sideTwo + 1){
+                hypotenuse = sideOne * sideOne + sideTwo * sideTwo;
+
+                hypotenuse = sqrt(hypotenuse);
+                hypotenuseMod= fmodf(hypotenuse ,1);
+
+                if (hypotenuse > 500){
+                    break;
+                }
+                if (hypotenuseMod == 0){
+                    cout << sideOne << " " << sideTwo << " " << hypotenuse << endl;
+                }
+            }
+        }
+        break;
     }
 }
