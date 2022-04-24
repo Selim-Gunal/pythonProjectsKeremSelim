cf3b4cdaa3b60f0cf7973856731f9beabc428563
Selim G <selim.g.2007"gmail.com>
2022-04-24 13:30:05 +0300

    JAVA

---
+++ 1.java
@@ -0,0 +1,22 @@
+package com.firstProject;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i, sum;
+        sum = 0;
+        for (i = 1 ;i < 1000 ;i = i + 1){
+            if (i % 3 == 0){
+                sum = sum + i;
+            }
+
+            if (i % 5 == 0) {
+                sum = sum + i;
+                if (i % 3 == 0) {
+                    sum = sum - i;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
