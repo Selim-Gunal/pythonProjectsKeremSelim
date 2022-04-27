a83abae67673c37b6b421434f5a998e0d1fbd4f1
Selim G <selim.g.2007"gmail.com>
2022-04-27 20:59:59 +0300

    JAVA

---
+++ Second.java
@@ -0,0 +1,25 @@
+package com.firstProject;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum;
+        int a ,b ;
+        a = 1;
+        b = 2;
+        sum = 0;
+
+        while (a <= 4000000 & b <= 4000000){
+            if (a % 2 == 0){
+                sum = sum + a;
+            }
+            if (b % 2 == 0){
+                sum = sum + b;
+            }
+            a = a + b;
+            b = a + b;
+        }
+
+        System.out.println(sum);
+    }
+}
