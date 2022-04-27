74a24a731b826f5cc414c6334b880ea4b2f077d6
Selim G <selim.g.2007"gmail.com>
2022-04-27 22:20:37 +0300

    JAVA

--- Third.java
+++ Third.java
@@ -3,6 +3,26 @@
 
 public class Third {
     public static void main(String[] args){
+        long i ,x ;
+        boolean indicator ,enterToIf;
 
+        //The "L" represents type long because if we didn't assign "L" it means it is a 32-Bit integer after we add "L" it becomes 64-Bit integer
+        for (i = 2 ;i < 600851475143L ;i = i + 1){
+            indicator = false;
+            enterToIf = false;
+
+            if (600851475143L % i == 0){
+                enterToIf = true;
+                for (x = 2 ;x < i ;x = x + 1){
+                    if (i % x == 0){
+                        indicator = true;
+                        break;
+                    }
+                }
+            }
+            if (indicator == false & enterToIf){
+                System.out.println(i);
+            }
+        }
     }
 }
