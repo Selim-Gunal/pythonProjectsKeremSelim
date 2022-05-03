b9d6a856b18bbc34972131a33fc78e8046036d8a
Selim G <selim.g.2007"gmail.com>
2022-05-03 14:17:35 +0300

    cpp

--- 5_23.cpp
+++ 5_23.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main() {
-    int i ,x ,hashtag ,hashtagCache ,star ,starCache ,whatLine;
+    int i ,x ,hashtag ,hashtagCache ,star ,starCache ,whatLine ,space ,spaceCache;
     bool starIncreasePoint;
     whatLine = 0;
     starCache = 8;
     starIncreasePoint = false;
+    spaceCache = 0;
     for (i = 0 ;i < 9 ;i = i + 1){
         if (whatLine == 0 or whatLine == 8){
             for (hashtagCache = 0 ;hashtagCache < 8 ;hashtagCache = hashtagCache + 1){
@@ -16,6 +17,20 @@ int main() {
         }
             else {
             cout << "#";
+        }
+
+        if (whatLine > 1 && whatLine < 7){
+            if (whatLine < 5){
+                spaceCache = spaceCache + 1;
+            }
+
+            for (space = 0 ;space < spaceCache ;space = space + 1){
+                cout << " ";
+            }
+
+            if (whatLine >= 4){
+                spaceCache = spaceCache - 1;
+            }
         }
 
         if (0 < whatLine && whatLine < 8){
