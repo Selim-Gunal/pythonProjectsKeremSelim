70345f7ffd5e9e44b1a650f6ccdfa49ab5ef6fa2
Selim G <selim.g.2007"gmail.com>
2022-05-04 22:09:19 +0300

    JAVA

--- Forth.java
+++ Forth.java
@@ -5,10 +5,15 @@ public class Forth {
         StringBuilder cacheReverse;
 
         for (i = 999 ;i > 99 ;i = i - 1){
-            for (x = 999 ;i > 99 ;i = i - 1){
+            for (x = i ;x > 99 ;x = x - 1){
                 multiply = i * x;
                 convertedToStr = String.valueOf(multiply);
+                cacheReverse = new StringBuilder(convertedToStr);
+                reversed = new String(cacheReverse);
 
+                if (convertedToStr == reversed){
+                    System.out.println(convertedToStr);
+                }
             }
         }
     }
