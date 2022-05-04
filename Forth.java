f92287c2c2ab56ebeb0165ff6c0d9e6bb70b8d2b
Selim G <selim.g.2007"gmail.com>
2022-05-04 22:39:40 +0300

    JAVA

--- Forth.java
+++ Forth.java
@@ -1,8 +1,9 @@
 public class Forth {
     public static void main(String[] args){
-        int i ,x ,multiply;
+        int i ,x ,multiply ,biggest;
         String convertedToStr ,reversed;
         StringBuilder cacheReverse;
+        biggest = 1;
 
         for (i = 999 ;i > 99 ;i = i - 1){
             for (x = i ;x > 99 ;x = x - 1){
@@ -11,10 +12,13 @@ public class Forth {
                 cacheReverse = new StringBuilder(convertedToStr);
                 reversed = new String(cacheReverse);
 
-                if (convertedToStr == reversed){
+                if (!(convertedToStr.equals(reversed))){
                     System.out.println(convertedToStr);
+                    System.out.println(reversed);
+
                 }
             }
         }
+        System.out.println(biggest);
     }
 }
