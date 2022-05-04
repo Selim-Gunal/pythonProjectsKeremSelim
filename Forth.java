58474845b462654301ff993f51192eba6024b202
Selim G <selim.g.2007"gmail.com>
2022-05-04 21:50:10 +0300

    JAVA

--- Forth.java
+++ Forth.java
@@ -1,5 +1,15 @@
 public class Forth {
     public static void main(String[] args){
+        int i ,x ,multiply;
+        String convertedToStr ,reversed;
+        StringBuilder cacheReverse;
 
+        for (i = 999 ;i > 99 ;i = i - 1){
+            for (x = 999 ;i > 99 ;i = i - 1){
+                multiply = i * x;
+                convertedToStr = String.valueOf(multiply);
+
+            }
+        }
     }
 }
