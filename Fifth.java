4a6a8331303735b5e49bc6eeeaf7dc65db60d7b2
Selim G <selim.g.2007"gmail.com>
2022-05-06 00:20:57 +0300

    JAVA

---
+++ Fifth.java
@@ -0,0 +1,19 @@
+public class Fifth {
+    public static void main(String[] args){
+        int i ,x ,cachePrimeNumber;
+        boolean primeNumberTest;
+        for (i = 2 ;i < 21 ;i = i = 1){
+            primeNumberTest = false;
+            cachePrimeNumber = i;
+            for (x = 2 ;x < 21 ;x = x + 1){
+                if (i % x == 0){
+                    primeNumberTest = true;
+                    break;
+                }
+            }
+            if (primeNumberTest == false){
+                if ()
+            }
+        }
+    }
+}
