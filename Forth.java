18e48617a4119a710cf604ccce5df52de34654d6
Selim G <selim.g.2007"gmail.com>
2022-05-06 13:16:56 +0300

    added comments

--- Forth.java
+++ Forth.java
@@ -1,5 +1,6 @@
 public class Forth {
     public static void main(String[] args){
+        //I assign the variables
         int i ,x ,multiply ,biggest;
         String convertedToStr ,reversed;
         StringBuilder cacheReverse;
@@ -8,11 +9,16 @@ public class Forth {
         for (i = 999 ;i > 99 ;i = i - 1){
             for (x = i ;x > 99 ;x = x - 1){
                 multiply = i * x;
+                //I convert integer to string
                 convertedToStr = String.valueOf(multiply);
+                //I covert convertedToStr to StringBuilder and then assign to cacheReverse
                 cacheReverse = new StringBuilder(convertedToStr);
+                //Here I reverse the cacheReverse
                 cacheReverse.reverse();
+                //And then I convert back to string then assing to reversed
                 reversed = new String(cacheReverse);
 
+                //Here I questioned why we can not do covertedToStr == reversed
                 if (convertedToStr.equals(reversed)){
                    if (multiply > biggest){
                        biggest = multiply;
@@ -23,3 +29,4 @@ public class Forth {
         System.out.println(biggest);
     }
 }
+//The expected result if 906609
