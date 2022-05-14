592f4a01e53491ccde30fc5087d611d5347ffefd
Selim G <selim.g.2007"gmail.com>
2022-05-14 23:52:52 +0300

    added comments

--- Sixth.java
+++ Sixth.java
@@ -1,3 +1,8 @@
+//The sum of the squares of the first ten natural numbers is,
+//The square of the sum of the first ten natural numbers is,
+//Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 public class Sixth {
     public static void main(String[] args){
         int i ,sumOfSquares ,squareOfSum ,squareOfSumResult;
